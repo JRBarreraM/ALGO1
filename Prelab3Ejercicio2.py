25fa4f031760e73d7ba4d0f7c7e98f0a71cd06ac
JRBarreraM <35788695+JRBarreraM@users.noreply.github.com>
2018-01-27 16:03:27 -0400

    Add files via upload

--- Prelab3Ejercicio2.py
+++ Prelab3Ejercicio2.py
@@ -4,51 +4,38 @@
 		
 	Autor: 
 		Br. Jose Barrera
-	Ultima modificacion: 25/01/2018
+	Ultima modificacion: 27/01/2018
 	VARIABLES:
 	N: int // ENTRADA: Numero al que se le contaran los divisore.
-	cuenta: int // SALIDA: Si es bisiesto o no.
-	i: int // SALIDA: ?.
+	i: int // Valor que permite recorrer los enteros entre 1 y N.
+	cota: int // Valor de la cota decreciente que permitirá que el ciclo termine
+	cuenta: int // SALIDA: Numero de divisores.
+	
 """
 
 # Valores iniciales:
-N = int(input("ingrese el numero = "))
+N=int(input("Ingrese el valor de N: "))
 
 # Precondicion: 
-{N > 0}
+assert(N > 0)
 
-# Calculos:
-(cuenta, i) = (0, 1)
+# Inicializaciones del ciclo
+cuenta, i = 0, 1
+cota = N - i + 2
+
+# Verificacion de invariante y cota al inicio
+assert( (0<i<=N+1) and (cuenta == sum ( j for j in range(1,i) if (N % j ==0) ) ) )
+assert(cota >= 0)
+
 while i<=N:
-	if N % i == 0:
-		cuenta = cuenta + 1
+	if (N % i) == 0:
+		cuenta += 1
 	else:
 		pass
-	i = i + 1
+	i += 1
 
 # Postcondicion: 
-{cuenta == (sum j for j in range( 0 < j <= N ) if N % j == 0 )}
+assert(cuenta == sum ( 1 for j in range(1,N+1) if (N % j ==0) ) )
 
 # Salida:
-print(cuenta)
-"""[
-const N: int;
-var cuenta: int;
-var i: int;
-{N > 0}
-cuenta,i := 0,1;
-{inv 0<i<=N+1 /\ cuenta=(%count j: 0<j<i: N mod j = 0 )}
-{bound N-i+2 }
-do ( i<=N ) ->
-if ( N mod i = 0 ) ->
-cuenta := cuenta + 1;
-[] ( N mod i != 0 )
-skip
-fi;
-i = i+1
-od
-{cuenta = (%count j: 0<j<= N : N mod j = 0 )}
-]
-
-(%oount j: 0<j<= N: N mod j=0)=(%sigma j: 0<j<= N /\ N mod j = 0: 1 )
-"""
+print( "Existen",cuenta,"divisores para",N)
