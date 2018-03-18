079cb43518503d5d97adf142c21273f7ccea3ef1
JRBarreraM <35788695+JRBarreraM@users.noreply.github.com>
2018-03-18 11:23:15 -0400

    Versión Final

--- PreLab8Ejercicio1.py
+++ PreLab8Ejercicio1.py
@@ -2,12 +2,19 @@
 	DESCRIPCION: Programa que lee un archivo de nombre “input.txt” con el formato\
   	(un número entero y un string por línea) para una cadena ADN factorizada. Y \
   	escribe en otro archivo de nombre “output.txt”, el resultado del ADN no-factorizado.
-Autor: 
-	Br. Jose Barrera
-Ultima modificacion: 18/03/2018
+	Autor: 
+		Br. Jose Barrera
+	Ultima modificacion: 18/03/2018
+		VARIABLES:
+	A: int // Lista de tuplas donde el primer elemento es el factor del segundo.
 """
 
-def ADN(l=list) -> bool:
+# Descripcion: Funcion verifica si todos los elementos de un str son bases del ADN.
+# Parametros:
+def ADN(l=str) -> bool:
+# PRECONDICION: True
+# var L: int // tamaño del str de entrada
+# var R: int // contador de bases del ADN en los elementos del str
 	L=len(l)
 	R=0
 	for i in range(L):
@@ -16,26 +23,32 @@ def ADN(l=list) -> bool:
 	if R==L:
 		return True
 	return False
+# POSTCONDICION: (%forall i : 0<=i<L : (l[i] == "A") or (l[i] == "T") or (l[i] == "G") or (l[i] == "C"))=>True
 
+##################################
+# Inicio del programa 
+##################################
+
+# Valores iniciales:
 with open("input.txt") as f:
 	for line in f:
-		print(line, end="")
+		print(line, end="")							#se muestra el contenido del archuvo input.txt al usuario
 	print("\n")
 f.closed
 
 with open("input.txt") as f:
-		A=[line.split() for line in f]
+		A=[line.split() for line in f]				#se separan los factores de las Bases
 f.closed
 
 with open("output.txt", "r+") as f:
 	for i in range(len(A)):
 		if 0<int(A[i][0])<50 and ADN(A[i][1]):
-			f.write(A[i][1]*int(A[i][0])+"\n")		
+			f.write(A[i][1]*int(A[i][0])+"\n")		#se  desfactoriza el ADN si se cumplan las condiciones		
 		else:
-			f.write("Linea erronea\n")
+			f.write("Linea erronea\n")				#si  no se cumplen las condiciones se escribe "Linea erronea"
 f.closed
 
 with open("output.txt", "r+") as f:
 	for line in f:
-		print(line, end="")
+		print(line, end="")							#se muestra el contenido del archuvo output.txt al usuario
 f.closed
