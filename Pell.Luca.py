bd3413530b56ac2ef7810ff60b1132d87f41d36d
JRBarreraM <35788695+JRBarreraM@users.noreply.github.com>
2018-04-10 11:09:24 -0400

    Pelluca Iterativo y Recursivo

--- Pell.Luca.py
+++ Pell.Luca.py
@@ -9,14 +9,11 @@ Razonamiento:
 	i,p,s=1,1,0
 						(n,i+1,(2*p)+s,p) si n>i
 				}														
-
-
 																		p(5,1,,)
 																		p(5,2,5,12)
 																		p(5,3,2,5)
 																		p(5,4,1,2)
 																		p()
-
 				Pell.Luca(0,0,1)										Pell.Luca(n,i,p,s)
 																		Pell.Luca(5,1,1,0)
 Q(2)=2*Q(1)+Q(0)=2*2+2=6												Pell.Luca(5,2,2,1)
@@ -29,7 +26,18 @@ Q(5)=2*Q(4)+Q(13)=2*(12*Q(1)+5*Q(0))+(5Q(1)+2Q(0))=29*Q(1)+12*Q(0)		Pell.Luca(5,
 
 # Pell_Luca iterativo.
 
-
+def Pell_LucaI(n=int)->int:
+	#VAR
+		# i,p,s,var: int
+	i,p,s=1,1,0
+	if n==0:
+		return 2
+	while n>=i:
+		aux=p
+		p=2*p+s
+		s=aux	
+		i=i+1
+	return 2*(p+s)
 
 # Pell_Luca recursivo de Cola.
 def Pell_Luca(n=int,i=int,p=int,s=int)->int:
@@ -48,8 +56,12 @@ print("Si se le introducen los valores Pell_Luca(n,1,1,0)")
 while True:
 	try:
 		n=int(input("Ingrese n para comprobarlo "))
-		assert(0<=n<=998)
+		opcion=int(input("iterativo=1 , recursivo de cola = 2  "))
+		assert((opcion==1 and 0<=n<=99999) or (opcion==2 and 0<=n<=998))
 		break
 	except:
-		print("n debe ser un entero mayor que 0 y menor que 999")
-print(Pell_Luca(n,1,1,0))
+		print("n debe ser un entero mayor que 0 y menor que 999(recursivo) y menor 99999(iterativo)")
+if opcion==2:
+	print(Pell_Luca(n,1,1,0))
+elif opcion==1:
+	print(Pell_LucaI(n))
